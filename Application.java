983f8810b58e7227b38fc75af7210395a5a0fd40
jagadeesh93461 <konetijagadeesh7@gmail.com>
2025-06-28 15:03:32 +0530

    Update Application.java

--- Application.java
+++ Application.java
@@ -1,1 +1,22 @@
 git demo project in jenkins
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Hello World</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      background-color: #f0f8ff;
+      text-align: center;
+      margin-top: 100px;
+    }
+
+    h1 {
+      color: #007acc;
+    }
+  </style>
+</head>
+<body>
+  <h1>Hello, World!</h1>
+</body>
+</html>
